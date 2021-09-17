1af30112cc799a0fa1e2d8403b580a9aa5d93dce
hieipad <89884542+hieipad@users.noreply.github.com>
2021-09-17 09:40:36 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '@summer0732',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '浅草寺',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '五重塔',
           // q2: 'かかか',
           // q3: 'ききき',
         }
